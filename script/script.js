f77772b5c09fac7e42a2d1f50d1a8b84f8fd3604
lyukidon <qgx99098@gmail.com>
2021-05-06 19:38:54 +0900

    인원 수 설정하는 부분 버튼 클릭 시 숫자가 변하도록 만들음

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+        // 방 종류 별로 가격 나오는 부분
 let roomSizeArr = [...document.querySelectorAll('.roomSelected')]
 let roomFeeArr = ['100,000', '200,000', '300,000', '400,000'];
 
@@ -6,6 +7,31 @@ for (let i=0;i<roomSizeArr.length;i++){
         for (let i=0;i<roomSizeArr.length;i++){
             if (this === roomSizeArr[i]){
                 document.querySelector('#roomFeeBox').innerText = roomFeeArr[i];
+            }
+        }
+    })
+}
+        // 인원 수
+let countButton = [...document.querySelectorAll('button.countButton')];
+for (let i=0;i<countButton.length;i++){
+    countButton[i].addEventListener('click',function(){
+        if(this.parentNode === document.querySelector('div#adult')){
+            if (document.querySelector('input#adultCount').value <= 0) {
+                if(this.innerText === '+'){
+                    document.querySelector('input#adultCount').value++;
+                }
+            }else{
+                this.innerText === '-' ?
+                document.querySelector('input#adultCount').value-- : document.querySelector('input#adultCount').value++;
+            }
+        }else{
+            if (document.querySelector('input#childCount').value <= 0) {
+                if(this.innerText === '+'){
+                    document.querySelector('input#childCount').value++;
+                }
+            }else{
+                this.innerText === '-' ?
+                document.querySelector('input#childCount').value-- : document.querySelector('input#childCount').value++;
             }
         }
     })
